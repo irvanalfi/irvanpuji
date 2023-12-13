90f9e1a72adf73a2a2015267187d7dbc378d0f52
Falrosyid <fatwa.alrosyid28@gmail.com>
2023-12-13 16:00:35 +0700

    hit api with ajax jquery

--- curl.php
+++ curl.php
@@ -1,8 +1,6 @@
 <?php
-
 $curl = curl_init();
 
-// $url    = 'http://alt.office.gilitransfers.com/api/get-fast-boat-order';
 $url    = 'https://api.sandbox.gilitransfers.com/wishes/messages';
 curl_setopt_array($curl, array(
     CURLOPT_URL => $url,
@@ -11,8 +9,6 @@ curl_setopt_array($curl, array(
 ));
 
 $response       = curl_exec($curl);
-// $result         = json_decode($response, true);
+$result         = json_decode($response, true);
 curl_close($curl);
 return $response;
-
-?>
